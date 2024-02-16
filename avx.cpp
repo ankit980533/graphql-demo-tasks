404cc3bace39ad8788589027c2fceb28ddbb82bf
ankit980533 <77059477+ankit980533@users.noreply.github.com>
2024-02-16 15:12:02 +0530

    rate limiter

--- avx.cpp
+++ avx.cpp
@@ -1,29 +1,9 @@
-// CPP program to find the x in range [l, r]
-// such that x ^ n is maximum.
+
 #include <cmath>
 #include <iostream>
 using namespace std;
  
-// Function to calculate the maximum value of
-// N ^ X, where X is in the range [L, R]
-// int maximumXOR(int n, int l, int r)
-// {
-//     int x = 0;
-//     for (int i = log2(r); i >= 0; --i) 
-//     {
-//         if (n & (1 << i))  // Set bit
-//         { 
-//             if (x + (1 << i) - 1 < l)
-//                 x ^= (1 << i);
-//         }
-//         else // Unset bit
-//         { 
-//             if ((x ^ (1 << i)) <= r)
-//                 x ^= (1 << i);
-//         }
-//     }
-//     return n ^ x;
-// }
+
  int max_XOR(int N, int L, int R) {
     int max_XOR_val = 0;
     for (int X = L; X <= R; X++) {
--- index.js
+++ index.js
@@ -2,6 +2,50 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
 const app = express();
+const rateLimits={};
+const MAX_LIMIT=10;
+const WindowMs=10*60*1000;
+
+function rateLimiter(req,res,next){
+    const now=Date.now();
+    // console.log(now);
+    const ip=req.ip;
+    if(!rateLimits[ip]){
+        rateLimits[ip]={
+       request:1,
+       startTime:now
+        }
+        return next();
+    }
+
+    const deltaTime=Date.now()-rateLimits[ip].startTime ;
+    if(WindowMs>deltaTime){
+        rateLimits[ip].request++;
+        if(rateLimits[ip].request > MAX_LIMIT){
+            const response={
+                msg:"Limit exceeds",
+                statusCode:res.statusCode
+            }
+            return res.status(429).json(response);
+        }
+        return next();
+    }
+
+
+    rateLimits[ip]={
+        request:1,
+        startTime:Date.now()
+         }
+
+
+         return next();
+
+
+
+}
+app.use(rateLimiter);
+
+
 app.use(
   '/graphql',
   graphqlHTTP((req) => ({
